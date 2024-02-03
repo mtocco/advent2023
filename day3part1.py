5949e0d3e381d58b13e8ab5e65780262651ec993
Michael Tocco <michaeledwardtocco@gmail.com>
2024-02-02 23:28:15 -0800

    Refactor day3part1.py

--- day3part1.py
+++ day3part1.py
@@ -4,6 +4,14 @@
 #           https://www.geeksforgeeks.org/python-remove-empty-strings-from-list-of-strings/
 import re
 
+def get_neighbor_chars(line, index):
+    neighbor_chars = ""
+    if index > 0:
+        neighbor_chars += line[index - 1]
+    if index < len(line) - 1:
+        neighbor_chars += line[index + 1]
+    return neighbor_chars
+
 def main ():
     f = open("dataPart1.txt", "r")
     lineList = f.readlines()
@@ -20,75 +28,26 @@ def main ():
         for index, char in enumerate(line):
             if char.isdigit():
                 if numericToggle: continue
-                if (bool(re.match(r'^[^\d\n.]$', str(line[index-1])))):
-                    testCase.append(sanitizeList[currentSanatizeIndex])
-                    currentSanatizeIndex += 1
-                    numericToggle = True
-                    continue
-                if (bool(re.match(r'^[^\d\n.]$', str(line[index+1])))):
-                    testCase.append(sanitizeList[currentSanatizeIndex])
-                    currentSanatizeIndex += 1
-                    numericToggle = True
-                    continue
 
+                testChars = get_neighbor_chars(line, index)
                 if previous_line is not None:
-                    charAbove = previous_line[index]
-                    try:
-                        charAboveRight = previous_line[index + 1]
-                    except IndexError:
-                        charAboveRight = None
-                    try:
-                        charAboveLeft = previous_line[index - 1]
-                    except IndexError:
-                        charAboveLeft = None
-                    if (bool(re.match(r'^[^\d\n.]$', str(charAbove)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
-                    if charAboveRight is not None and (bool(re.match(r'^[^\d\n.]$', str(charAboveRight)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
-
-                    if charAboveLeft is not None and (bool(re.match(r'^[^\d\n.]$', str(charAboveLeft)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
+                    testChars += get_neighbor_chars(previous_line, index)
             
                 if next_line is not None:
-                    charBelow = next_line[index]
-                    try:
-                        charBelowRight = next_line[index + 1]
-                    except IndexError:
-                        charBelowRight = None
-                    try:
-                        charBelowLeft = next_line[index - 1]
-                    except IndexError:
-                        charBelowLeft = None
-                    if (bool(re.match(r'^[^\d\n.]$', str(charBelow)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
-                    if charBelowRight is not None and (bool(re.match(r'^[^\d\n.]$', str(charBelowRight)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
-                    if charBelowLeft is not None and (bool(re.match(r'^[^\d\n.]$', str(charBelowLeft)))):
-                        testCase.append(sanitizeList[currentSanatizeIndex])
-                        currentSanatizeIndex += 1
-                        numericToggle = True
-                        continue
+                    testChars += get_neighbor_chars(next_line, index)
+                
+                contains_invalid_chars = any(re.match(r'[^\d\n.]', char) for char in testChars)
+                if contains_invalid_chars:
+                    testCase.append(sanitizeList[currentSanatizeIndex])
+                    currentSanatizeIndex += 1
+                    numericToggle = True
+                
+                testChars = ""
             else:
-                if index > 0 and line[index - 1].isdigit() and not numericToggle:
+                if index >= 0 and line[index - 1].isdigit() and not numericToggle:
                     currentSanatizeIndex += 1
                 numericToggle = False
         previous_line = line
     print("Sum is: " + str(sum(testCase)))
-
         
 main()
